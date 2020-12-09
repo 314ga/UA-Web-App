b916e4ba59aa42d96b5769ac49ba4ebc0c1c1cf7
314ga <jakub.piga.cp@gmail.com>
2020-12-09 18:48:44 +0100

    Static page problem

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
