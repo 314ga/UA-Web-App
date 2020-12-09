5df5328fb2c5ea43ae29ae5c9cc9b5dbb03485f5
314ga <jakub.piga.cp@gmail.com>
2020-12-09 16:11:44 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import FlightsPage from './components/FlightsPage';
+import ManufacturerPage from './components/ManufacturerPage';
+import WeatherPage from './components/WeatherPage';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const value = 'kub';
-  return <div>Hello {value}</div>;
+  return(
+    <Router>
+      <Switch>
+        <Route exact path="/WeatherPage" component={WeatherPage} />
+        <Route exact path="/" component={FlightsPage} />
+        <Route exact path="/ManufacturerPage" component={ManufacturerPage} />
+
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
